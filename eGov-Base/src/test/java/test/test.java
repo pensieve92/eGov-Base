db04163d484df1be612b1cdf974a15be732ceb88
JeonBH <pensieve92@gmail.com>
2019-06-23 22:30:00 +0900

    1. 최초 업로드

---
+++ eGov-Base/src/test/java/test/test.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @since  : 2019. 6. 16.
+ * @author : JeonBH
+ * <PRE>
+ * -----------------------------
+ * 개정이력
+ * 2019. 6. 16. JeonBH : 최초작성
+ * </PRE> 
+ */
+public class test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+		//given
+		//TODO: 오늘은 여기까지
+		//TODO 여기까지
+		
+		
+
+		//work
+
+		//result
+
+	}
+
+}
